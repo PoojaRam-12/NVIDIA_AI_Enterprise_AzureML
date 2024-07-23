7230bbde76aed13bfb4d7bb321b9e729b5784829
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2024-07-23 13:53:59 +0530

    Update set_key_vault.sh

--- samples/cli/nemo/infer/llama3_8B_nim/scripts/set_key_vault.sh
+++ samples/cli/nemo/infer/llama3_8B_nim/scripts/set_key_vault.sh
@@ -3,8 +3,8 @@ set -x
 
 source scripts/config_files/deployment_config.sh
 
-echo "az keyvault create --name ${keyvault_name} --resource-group ${resource_group} --location ${location}"
-az keyvault create --name ${keyvault_name} --resource-group ${resource_group} --location ${location}
-az role assignment create --role "Key Vault Secrets User" --assignee ${email_address}  --scope "/subscriptions/${subscription_id}/resourceGroups/${resource_group}/providers/Microsoft.KeyVault/vaults/${keyvault_name}"
-az keyvault secret set --vault-name ${keyvault_name} --name "NGC-KEY" --value ${ngc_key}
-az keyvault secret show --vault-name ${keyvault_name}  --name "NGC-KEY"
+echo "az keyvault create --name ${nvidia-llama3-8b-nim-kv} --resource-group ${huggingface-registry-test1} --location ${eastus}"
+az keyvault create --name ${nvidia-llama3-8b-nim-kv} --resource-group ${huggingface-registry-test1} --location ${eastus}
+az role assignment create --role "Key Vault Secrets User" --assignee ${v-poojr@microsoft.com}  --scope "/subscriptions/${80c77c76-74ba-4c8c-8229-4c3b2957990c}/resourceGroups/${huggingface-registry-test1}/providers/Microsoft.KeyVault/vaults/${nvidia-llama3-8b-nim-kv}"
+az keyvault secret set --vault-name ${nvidia-llama3-8b-nim-kv} --name "nvidia-NGC-KEY" --value ${ngc_key}
+az keyvault secret show --vault-name ${nvidia-llama3-8b-nim-kv}  --name "nvidia-NGC-KEY"
