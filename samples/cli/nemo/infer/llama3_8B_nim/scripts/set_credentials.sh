9ec7482ff579d3d5f2fd16952afe3a9de2826c16
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2024-07-23 13:52:08 +0530

    Update set_credentials.sh

--- samples/cli/nemo/infer/llama3_8B_nim/scripts/set_credentials.sh
+++ samples/cli/nemo/infer/llama3_8B_nim/scripts/set_credentials.sh
@@ -3,7 +3,7 @@
 source scripts/config_files/deployment_config.sh
 az login
 
-az account set -s ${subscription_id} 
+az account set -s ${80c77c76-74ba-4c8c-8229-4c3b2957990c} 
 
-echo "az configure --defaults group=${resource_group} workspace=${workspace}"
-az configure --defaults group=${resource_group} workspace=${workspace}
+echo "az configure --defaults group=${huggingface-registry-test1} workspace=${test-eastus}"
+az configure --defaults group=${huggingface-registry-test1} workspace=${test-eastus}
