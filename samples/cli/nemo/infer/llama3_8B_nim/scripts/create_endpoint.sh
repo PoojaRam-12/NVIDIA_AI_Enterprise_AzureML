91406f8079e18246911ce8bd7143cfc14675ace6
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2024-07-23 13:54:56 +0530

    Update create_endpoint.sh

--- samples/cli/nemo/infer/llama3_8B_nim/scripts/create_endpoint.sh
+++ samples/cli/nemo/infer/llama3_8B_nim/scripts/create_endpoint.sh
@@ -4,9 +4,9 @@
 source scripts/config_files/deployment_config.sh
 
 cp scripts/auxiliary_files/endpoint_aml.yml scripts/auxiliary_files/actual_endpoint_aml.yml
-sed -i "s/endpoint_name_placeholder/${endpoint_name}/g" scripts/auxiliary_files/actual_endpoint_aml.yml
+sed -i "s/endpoint_name_placeholder/${nvidia-llama3-8b-nim-ep}/g" scripts/auxiliary_files/actual_endpoint_aml.yml
 
-echo "Creating Online Endpoint ${endpoint_name}"
+echo "Creating Online Endpoint ${nvidia-llama3-8b-nim-kv-ep}"
 az ml online-endpoint create -f scripts/auxiliary_files/actual_endpoint_aml.yml --resource-group $resource_group --workspace-name $workspace
 
 rm scripts/auxiliary_files/actual_endpoint_aml.yml
