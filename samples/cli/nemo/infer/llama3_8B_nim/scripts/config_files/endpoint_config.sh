f572ab1621bbab01317239ab87768d47c5b51b5f
PoojaRam-12 <141908731+PoojaRam-12@users.noreply.github.com>
2024-07-24 10:37:13 +0530

    Update endpoint_config.sh

--- samples/cli/nemo/infer/llama3_8B_nim/scripts/config_files/endpoint_config.sh
+++ samples/cli/nemo/infer/llama3_8B_nim/scripts/config_files/endpoint_config.sh
@@ -21,8 +21,12 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-endpoint_url="triton-tao-endpoint-aml-1.southcentralus.inference.ml.azure.com"
-endpoint_token="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+endpoint_url =""
+endpoint_token =""
+
+
+# endpoint_url="triton-tao-endpoint-aml-1.southcentralus.inference.ml.azure.com"
+# endpoint_token="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 
 
